dec7eab372884caa651de542c6764c424bde5427
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-26 22:49:16 +0100

    Display only 4 posts

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -5,7 +5,7 @@ const Post = props => (
   <Styled.Post>
     <h1>{props.title}</h1>
     <div>
-      <Styled.Author>Author</Styled.Author>
+      <Styled.Author>{props.author}</Styled.Author>
     </div>
   </Styled.Post>
 );
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -11,12 +11,17 @@ const Blog = () => {
 
   useEffect(() => {
     axios.get('http://jsonplaceholder.typicode.com/posts')
-      .then(response => setPosts(response.data));
+      .then(response => setPosts(response.data.slice(0, 4)
+        .map(post => ({
+          ...post,
+          author: 'Igor'
+        }))));
   }, []);
 
   const postsDivs = posts.map(post => <Post
     key={post.id}
-    title={post.title}/>)
+    title={post.title}
+    author={post.author}/>)
 
   return (
     <div>
