8ccea2194cd262136e36d148402d5ac02eaa3ed3
Igor Maguskin <maguskin.igor@gmail.com>
2020-08-29 04:30:45 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(request => {
   console.log(request)
